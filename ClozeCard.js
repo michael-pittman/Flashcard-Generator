626a8d8d665a356349fcd7fb23a3ff23b8cddc67
michaelc.pittman <michaelc.pittman@gmail.com>
2017-07-27 12:26:53 -0400

    error var

--- ClozeCard.js
+++ ClozeCard.js
@@ -2,9 +2,10 @@
 function ClozeCard(text, cloze, fulltext) {
   this.text = text;
   this.cloze = cloze;
-  this.fulltext = text + cloze
+  this.fulltext = text +" "+ cloze
 }
-var sky = new ClozeCard("the sky colour is ", "blue");
+var sky = new ClozeCard("the sky colour is", "blue");
+var brokenCloze = new ClozeCard("This doesn't work", "oops");
 
 console.log(sky.text); 
 console.log(sky.cloze); 
